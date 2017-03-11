fe427af3cd5d10ca1771fca1ea8d288381b76de4
jive_cc <jive_cc@3d569f3e-7048-4027-a931-fe1956ff214a>
2017-03-11 16:36:10 +0000

    HV: * underway to creating sokkits like we did in the python version!
    git-svn-id: svn+ssh://jop91.nfra.nl/export/home/jive_cvs/HAAVEE/etransfer@28 3d569f3e-7048-4027-a931-fe1956ff214a

--- src/etdc_fd.cc
+++ src/etdc_fd.cc
@@ -1,1 +1,43 @@
 // implementation of base- and derived classes for wrapping file descriptors
+//
+#include <etdc_fd.h>
+#include <reentrant.h>
+
+#include <stdexcept>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+namespace etdc {
+
+    // TCP sockets
+    etdc_tcp::etdc_tcp() {
+        std::cout << "etdc_tcp::etdc_tcp()" << std::endl;
+        auto proto = etdc::getprotobyname("tcp");
+        if( (__m_fd=::socket(PF_INET, SOCK_STREAM, proto.p_proto))==-1 )
+            throw std::runtime_error( "etdc_tcp: " + etdc::strerror(errno) );
+        std::cout << "  __m_fd=" << __m_fd << std::endl;
+    }
+
+    etdc_tcp::~etdc_tcp() {
+        std::cout << "etdc_tcp::~etdc_tcp()/__m_fd=" << __m_fd << std::endl;
+        if( __m_fd!=-1 )
+            ::close( __m_fd );
+    }
+
+    // UDT sockets
+    etdc_udt::etdc_udt() {
+        std::cout << "etdc_udt::etdc_udt()" << std::endl;
+        auto proto = etdc::getprotobyname("tcp");
+        if( (__m_fd=UDT::socket(PF_INET, SOCK_STREAM, proto.p_proto))==-1 )
+            throw std::runtime_error( "etdc_udt: " + etdc::strerror(errno) );
+        std::cout << "  __m_fd=" << __m_fd << std::endl;
+    }
+
+    etdc_udt::~etdc_udt() {
+        std::cout << "etdc_udt::~etdc_udt()/__m_fd=" << __m_fd << std::endl;
+        if( __m_fd!=(UDTSOCKET)-1 )
+            UDT::close( __m_fd );
+    }
+}
--- src/etdc_fd.h
+++ src/etdc_fd.h
@@ -23,72 +23,22 @@
 // Own includes
 #include <notimplemented.h>
 #include <utilities.h>
+#include <etdc_setsockopt.h>
+#include <etdc_resolve.h>
+#include <construct.h>
+#include <reentrant.h>
 #include <tagged.h>
+#include <udt.h>
 
 // C++
+#include <map>
 #include <tuple>
+#include <memory>
 #include <string>
-
-#if 0
 #include <iostream>
-#include <sstream>
-#include <string>
 
-#include <sys/time.h>   // struct timeval
-#include <sys/socket.h> // SOL_SOCKET
+#include <sys/socket.h>
 
-template <class CharT, class Traits>
-std::basic_ostream<CharT, Traits>&  operator<<(std::basic_ostream<CharT, Traits>& os, struct timeval const& tv) {
-    return os << "struct timeval{.tv_sec=" << tv.tv_sec << ", .tv_usec=" << tv.tv_usec << "}";
-}
-#endif
-#if 0
-namespace cruft {
-    struct host_tag {};
-    struct protocol_tag {};
-    struct port_tag {};
-    struct name_tag {};
-    struct level_tag {};
-}
-using host_type     = tagged<std::string, cruft::host_tag>;
-using protocol_type = tagged<std::string, cruft::protocol_tag>;
-
-template <int Name>
-using name_type     = tagged<std::integral_constant<int,Name>, cruft::name_tag>;
-template <int Level>
-using level_type    = tagged<std::integral_constant<int,Level>, cruft::level_tag>;
-
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-using socket_level  = level_type<SOL_SOCKET>;
-using tcp_level     = level_type<IPPROTO_TCP>;
-
-using sndbuf_type   = tagged<int, level_type<SOL_SOCKET>, name_type<SO_SNDBUF>>;
-using rcvbuf_type   = tagged<int, level_type<SOL_SOCKET>, name_type<SO_RCVBUF>>;
-using rcvbufd_type  = tagged<double, level_type<SOL_SOCKET>, name_type<SO_RCVBUF>>;
-using rcvtimeo_type = tagged<struct timeval, name_type<SO_RCVTIMEO>, level_type<SOL_SOCKET>>;
-using tcp_nodelay   = tagged<int, tcp_level, name_type<TCP_NODELAY>>;
-
-template <template <typename...> class Pred, typename...>
-struct get_tag_p {
-    using type = std::tuple<>;
-};
-
-template <template <typename...> class Pred, typename T, typename... Tags>
-struct get_tag_p<Pred, tagged<T, Tags...>> {
-    using result_type  = typename etdc::apply<Pred, Tags...>::type;
-
-    using type = typename std::conditional<etdc::has_type<std::true_type, result_type>::value,
-                                           typename std::tuple_element<etdc::index_of<std::true_type, result_type>::value, std::tuple<Tags...>>::type,
-                                           std::tuple<>>::type;
-};
-
-
-    std::cout << "level = " << get_tag_p<has_level_tag, tcp_nodelay>::type::type::value << std::endl;
-    std::cout << "level = " << get_tag_p<has_level_tag, rcvtimeo_type>::type::type::value << std::endl;
-
-#endif
-#include <iostream>
 
 // Make <host> and <protocol> constructible from std::string (and usable as ~)
 // but you cannot mix them - they become their own type
@@ -97,16 +47,16 @@ namespace etdc {
     class protocol_type : public std::string { using std::string::string; };
     //enum class port_type: unsigned short { any = 0 };
     namespace tags {
-        struct port_tag {};
+        struct mtu_tag     {};
+        struct port_tag    {};
+        struct backlog_tag {};
     }
 
-    using port_type = etdc::tagged<unsigned short, tags::port_tag>;
+    using mtu_type     = etdc::tagged<unsigned int, tags::mtu_tag>; // MTU<0 don't make sense
+    using port_type    = etdc::tagged<unsigned short, tags::port_tag>;
+    using backlog_type = etdc::tagged<int, tags::backlog_tag>;
     static constexpr port_type any_port = port_type{ (unsigned short)0 };
-#if 0
-    std::ostream& operator<<(std::ostream& os, port_type const& p) {
-        return os << static_cast<unsigned short>(p);
-    }
-#endif
+
     // ipport_type:   <host> : <port>
     // sockname_type: <type> : <host> : <port>
     using ipport_type   = std::tuple<host_type, port_type>;
@@ -116,6 +66,8 @@ namespace etdc {
     class etdc_fd {
 
         public:
+            int __m_fd {};
+
             // We pretend to be just an interface
             etdc_fd() {}
             virtual ~etdc_fd() {}
@@ -136,13 +88,13 @@ namespace etdc {
             virtual off_t         size( void ) NOTIMPLEMENTED;
             virtual int           close( void ) NOTIMPLEMENTED;
 
+            virtual int           connect( ipport_type const& ) NOTIMPLEMENTED;
+
             // server-style interface
             virtual int           bind( ipport_type const& ) NOTIMPLEMENTED;
             virtual int           listen( int ) NOTIMPLEMENTED;
             virtual etdc_fd*      accept( void ) NOTIMPLEMENTED;
     };
-#undef NOTIMPLEMENTED
-#undef THISFUNCTION
 
     //////////////////////////////////////////////////////////////////
     //
@@ -154,10 +106,30 @@ namespace etdc {
         public etdc_fd
     {
         public:
-            etdc_tcp() {}
-            virtual ~etdc_tcp() {}
+            etdc_tcp();
+            virtual ~etdc_tcp();
 
+            //int __m_fd {};
     };
+
+    class etdc_udt:
+        public etdc_fd
+    {
+        public:
+            // For the data channel we want BIG buffers, typically!
+            // these are defaults and can be overridden by the usert
+            //constexpr static int bufSize = {320*1024*1024};
+            //constexpr extern static int bufSize = {320*1024*1024};
+            //const static int bufSize = 320*1024*1024;
+
+            etdc_udt();
+
+            virtual ~etdc_udt();
+
+            //UDTSOCKET __m_fd;
+    };
+
+
 // End of the etdc namespace
 }
 
@@ -169,13 +141,11 @@ namespace etdc {
 
 // Functions to create an instance
 template <typename T>
-//etdc::ipport_type mk_ipport(T const& host, etdc::port_type port = etdc::port_type::any) {
 etdc::ipport_type mk_ipport(T const& host, etdc::port_type port = etdc::any_port) {
     return etdc::ipport_type(host, port);
 }
 
 template <typename T, typename U>
-//etdc::sockname_type mk_sockname(T const& proto, U const& host, etdc::port_type port = etdc::port_type::any) {
 etdc::sockname_type mk_sockname(T const& proto, U const& host, etdc::port_type port = etdc::any_port) {
     return etdc::sockname_type(proto, host, port);
 }
@@ -234,6 +204,143 @@ etdc::protocol_type get_protocol(Ts... t) {
 }
 
 
+namespace etdc {
+    //////////////////////////////////////////////////////////////////
+    //
+    //                  Factories
+    //
+    //////////////////////////////////////////////////////////////////
 
+    using etdc_fdptr        = std::shared_ptr<etdc_fd>;
+
+    namespace detail {
+        //constexpr static int udtBufSize = {320*1024*1024};
+        constexpr static udt_rcvbuf defaultUDTBufSize{ 320*1024*1024 };
+
+        // For creating sokkits
+        using protocol_map_type = std::map<std::string, std::function<etdc_fdptr(void)>>;
+
+        static const  protocol_map_type protocol_map = { 
+            {"tcp", []() { return std::make_shared<etdc_tcp>(); }},
+            {"udt", []() { return std::make_shared<etdc_udt>(); }}
+        };
+
+
+        struct server_settings {
+            backlog_type     backLog    {};
+            ipport_type      srvAddress {};
+            etdc::so_rcvbuf  rcvBufSize {};
+            etdc::udt_rcvbuf udtBufSize {};
+        };
+        const etdc::construct<server_settings>  update_srv( &server_settings::backLog,
+                                                            &server_settings::srvAddress,
+                                                            &server_settings::rcvBufSize,
+                                                            &server_settings::udtBufSize );
+
+        using server_defaults_map = std::map<std::string, std::function<server_settings(void)>>;
+
+        static const server_defaults_map server_defaults = {
+            {"tcp", []() { return update_srv.mk(backlog_type{4}, mk_ipport("0.0.0.0") );}},
+            {"udt", []() { return update_srv.mk(backlog_type{4}, defaultUDTBufSize, mk_ipport("0.0.0.0"));}}
+            //{"udt", []() { return update_srv.mk(backlog_type{4}, etdc::udt_rcvbuf{etdc::etdc_udt::bufSize}, mk_ipport("0.0.0.0"));}}
+        };
+
+        // Basic transformation from plain socket into server or client socket
+        using xform_map_type = std::map<std::string, std::function<void(etdc_fdptr, detail::server_settings const&)>>;
+
+
+        // Default set of actions to turn a socket into a server socket
+        static const xform_map_type server_map = {
+            {"tcp", [](etdc_fdptr pSok, detail::server_settings const& srv) -> void {
+                        // Bind to ipport
+                        struct sockaddr_in sa;
+                        
+                        etdc::setsockopt(pSok->__m_fd, etdc::so_reuseaddr{true});
+
+                        // Need to resolve? For here we assume empty host means any
+                        if( !etdc::resolve_host<etdc::EmptyMeansAny>(get_host(srv.srvAddress), SOCK_STREAM, IPPROTO_TCP, sa) )
+                            throw std::runtime_error( std::string("Failed to resolve/tcp ")+get_host(srv.srvAddress) );
+
+                        // Get the port info
+                        // See "etdc_resolve.h" for FFS glibc shit why we
+                        // have to fucking manually wrap htons!!
+                        sa.sin_port = etdc::htons_( get_port(srv.srvAddress) );
+
+                        // Now we can bind(2)
+                        if( ::bind(pSok->__m_fd, (const struct sockaddr*)&sa, socklen_t(sizeof(struct sockaddr_in)))!=0 )
+                            throw std::runtime_error( std::string("Failed to bind/tcp - ")+etdc::strerror(errno) );
+
+                        // And also lissen(2)
+                        if( ::listen(pSok->__m_fd, 4)!=0 )
+                            throw std::runtime_error( std::string("Failed to listen/tcp - ")+etdc::strerror(errno) );
+                    }},
+            {"udt", [](etdc_fdptr pSok, detail::server_settings const& srv) -> void {
+                        // Bind to ipport
+                        struct sockaddr_in sa;
+
+                        etdc::setsockopt(pSok->__m_fd, etdc::so_reuseaddr{true},
+                                                       etdc::udt_reuseaddr{true},
+                                                       srv.udtBufSize);
+                        // Need to resolve? For here we assume empty host means any
+                        if( !etdc::resolve_host<etdc::EmptyMeansAny>(get_host(srv.srvAddress), SOCK_STREAM, IPPROTO_TCP, sa) )
+                            throw std::runtime_error( std::string("Failed to resolve/udt ")+get_host(srv.srvAddress) );
+
+                        // Get the port info
+                        // See "etdc_resolve.h" for FFS glibc shit why we
+                        // have to fucking manually wrap htons!!
+                        sa.sin_port = etdc::htons_( get_port(srv.srvAddress) );
+
+                        // Now we can bind(2)
+                        if( UDT::bind(pSok->__m_fd, (const struct sockaddr*)&sa, socklen_t(sizeof(struct sockaddr_in)))!=0 )
+                            throw std::runtime_error( std::string("Failed to bind/udt - ")+UDT::getlasterror().getErrorMessage() );
+
+                        // And also lissen(2)
+                        if( UDT::listen(pSok->__m_fd, 4)!=0 )
+                            throw std::runtime_error( std::string("Failed to listen/udt - ")+UDT::getlasterror().getErrorMessage() );
+                    }}
+        };
+        // Default id. actions to turn a socket into a client socket
+        static const xform_map_type client_map = {
+            {"tcp", [](etdc_fdptr /*pSok*/, detail::server_settings const&) {
+                    }},
+            {"udt", [](etdc_fdptr /*pSok*/, detail::server_settings const&) {
+                    }}
+        };
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//
+//                      These live in the global namespace again
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+etdc::etdc_fdptr mk_socket(T const& proto) {
+    auto pEntry = etdc::detail::protocol_map.find(proto);
+
+    if( pEntry==etdc::detail::protocol_map.end() )
+        throw std::runtime_error(std::string("mk_socket/No protocol entry found for protocol = ")+std::string(proto));
+    return pEntry->second();
+}
+
+
+template <typename T, typename... Ts>
+etdc::etdc_fdptr mk_server(T const& proto, Ts... ts) {
+    // Create socket and server defaults for the indicated protocol
+    auto pSok         = mk_socket(proto);
+    auto srvDefaults  = etdc::detail::server_defaults.find(proto)->second();
+    // Update the defaults with what the user may have given us
+    etdc::detail::update_srv(srvDefaults, std::forward<Ts>(ts)...);
+
+    // And now transform the server details + sokkit into a real servert
+    return etdc::detail::server_map.find(proto)->second(pSok, srvDefaults);
+#if 0
+    auto pSrvEntry    = etdc::detail::server_map.find(proto);
+    if( pEntry==etdc::detail::server_map.end() )
+        throw std::runtime_error(std::string("mk_server/No server entry found for protocol = ")+std::string(proto));
+    return pSrvEntry->second( pSok );
+#endif
+}
 
 #endif
