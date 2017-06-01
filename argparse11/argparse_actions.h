9c740f94763d86683f6990c17121867df2f1bdc5
jive_cc <jive_cc@3d569f3e-7048-4027-a931-fe1956ff214a>
2017-06-01 12:17:54 +0000

    HV: * add GPLv3 stuff
    git-svn-id: svn+ssh://jop91.nfra.nl/export/home/jive_cvs/HAAVEE/etransfer@67 3d569f3e-7048-4027-a931-fe1956ff214a

--- argparse11/argparse_actions.h
+++ argparse11/argparse_actions.h
@@ -1,6 +1,21 @@
-// Define the actions and support types to actually make command line options
+// Define the actions and support types/functionality to construct command line options
+// Copyright (C) 2017  Harro Verkouter, verkouter@jive.eu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef ARGPARSE11_ACTIONS_H
 #define ARGPARSE11_ACTIONS_H
+
 //
 //  The building blocks that are exposed to the user for building command
 //  line options:
--- argparse11/argparse_functools.h
+++ argparse11/argparse_functools.h
@@ -1,4 +1,18 @@
 // functional programming tools on std::tuple/std::array: head(), tail(), fold[lr](), filter_[ptv], map(), copy()
+// Copyright (C) 2017  Harro Verkouter, verkouter@jive.eu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef ARGPARSE_FUNCTOOLS_H
 #define ARGPARSE_FUNCTOOLS_H
 
