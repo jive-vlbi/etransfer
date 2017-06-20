c2322c6a3e71cd5b26d88305115f2e4ec2c9bb32
jive_cc <jive_cc@3d569f3e-7048-4027-a931-fe1956ff214a>
2017-06-20 13:15:01 +0000

    HV: * helper stuff to keep etransfer-server state
    git-svn-id: svn+ssh://jop91.nfra.nl/export/home/jive_cvs/HAAVEE/etransfer@92 3d569f3e-7048-4027-a931-fe1956ff214a

--- src/etdc_etd_state.h
+++ src/etdc_etd_state.h
@@ -1,17 +1,86 @@
 // etransfer daemon state object (shared)
+#ifndef ETDC_ETD_STATE_H
+#define ETDC_ETD_STATE_H
+
+// Own headers
+#include <etdc_fd.h>
+#include <etdc_uuid.h>
+#include <etdc_thread.h>
+
+// Standard C++ headers
+#include <map>
 #include <list>
 #include <mutex>
+#include <memory>
+#include <thread>
+#include <utility>
 #include <functional>
+
 
 namespace etdc {
 
-    using cancel_fn       = std::function<void(void)>;
-    using cancellist_type = std::list<cancel_fn>;
-    using scoped_lock     = std::lock_guard<std::mutex>;
+    // When requesting file access we want to restrict the options to this
+    enum class openmode_type : int {
+        omNew    = O_WRONLY | O_CREAT | O_EXCL,   // new-may-not-exist
+        omWrite  = O_WRONLY | O_TRUNC | O_CREAT,  // may-or-may-not-exist, will truncate
+        omAppend = O_WRONLY | O_CREAT | O_APPEND, // may-or-may-not-exist, if exist append at end
+        omRead   = O_RDONLY
+    };
+
+
+    // We keep per-transfer properties in here
+    struct transferprops_type {
+        std::string                 path;
+        etdc::etdc_fdptr            fd;
+        const openmode_type         openMode;
+        std::unique_ptr<std::mutex> lockPtr;
+
+        // we cannot be copied or default constructed! (because of our unique_ptr)
+        transferprops_type()                          = delete;
+        transferprops_type(transferprops_type const&) = delete;
+
+        // We could be moved, which we accomplish by swapping our default
+        // constructed elements with other's non-default ones, we assume
+        transferprops_type(transferprops_type&& other): openMode( other.openMode ) {
+            std::swap(path,    other.path);
+            std::swap(fd,      other.fd);
+            std::swap(lockPtr, other.lockPtr);
+        }
+
+        transferprops_type(etdc::etdc_fdptr efd, std::string const& p, openmode_type om):
+            path(p), fd(efd), openMode(om), lockPtr(new std::mutex())
+        {}
+    }; 
+
+    using cancel_fn         = std::function<void(void)>;
+    using cancellist_type   = std::list<cancel_fn>;
+    using scoped_lock       = std::lock_guard<std::mutex>;
+    using threadlist_type   = std::list<std::thread>;
+    using dataaddrlist_type = std::list<etdc::sockname_type>;
+    using transfermap_type  = std::map<etdc::uuid_type, transferprops_type>;
 
     // Keep global server state
     struct etd_state {
-        std::mutex      lock;
-        cancellist_type cancellations;
+        std::mutex           lock;
+        cancellist_type      cancellations;
+        threadlist_type      threads;
+        transfermap_type     transfers;
+        std::atomic<bool>    cancelled;
+        dataaddrlist_type    dataaddrs;
+
+        etd_state() : cancelled{ false }
+        {}
+
+        // To prevent deadlock we first construct the thread 
+        // and after the fact, grab a lock and modify the shared state
+        template <typename... Args>
+        void add_thread(Args&&... args) {
+            scoped_lock lk( lock );
+            // by using etdc::thread we make sure that the started thread 
+            // has all signals blocked
+            threads.emplace_back(etdc::thread(std::forward<Args>(args)...));
+        }
     };
 }
+
+#endif
---
+++ src/etdc_uuid.h
@@ -0,0 +1,48 @@
+// We use UUIDs to keep track of individual transfers
+#ifndef ETDC_UUID_H
+#define ETDC_UUID_H
+
+#include <etdc_assert.h>
+
+// C++ headers
+#include <mutex>
+#include <string>
+#include <random>
+#include <algorithm>
+#include <functional>
+
+namespace etdc {
+    class uuid_type     : public std::string {
+        public:
+            // We cannot have default uuids!
+            uuid_type()   = delete;
+
+            template <typename X, typename... Args>
+            uuid_type(X&& x, Args&&... args): std::string(std::forward<X>(x), std::forward<Args>(args)...) {
+                // After construction, assert that we're non-empty
+                ETDCASSERT(this->empty()==false, "UUID cannot be empty");
+            }
+
+            // Generate a new UUID
+            static uuid_type mk( void ) {
+                // In-class statics are a bugger. Jeebus.
+                static const std::string                                     __m_chars{ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345679" };
+                static std::mutex                                            __m_random_lock{};
+                static std::random_device                                    __m_random_device{};
+                static std::default_random_engine                            __m_random_engine{ __m_random_device() };
+                static std::uniform_int_distribution<std::string::size_type> __m_uniform_sizes{15, 20};
+                static std::uniform_int_distribution<std::string::size_type> __m_uniform_chars{0, __m_chars.size()};
+                static auto                                                  __m_sizegen = std::bind(__m_uniform_sizes, __m_random_engine);
+                static auto                                                  __m_chargen = [&]() { return __m_chars[__m_uniform_chars(__m_random_engine)]; };
+
+                // Non-static, automatic variables
+                std::string                 uuid;
+                std::lock_guard<std::mutex> mScopedLock( __m_random_lock );
+                
+                std::generate_n(std::back_inserter(uuid), __m_sizegen(), __m_chargen);
+                return uuid_type(uuid);
+            }
+    };
+}// namespace etdc
+
+#endif
